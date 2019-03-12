ad3f025900e100afc1e3fbac5f246c180d86a0e6
Eagan Martin <mece.martinece@gmail.com>
2019-03-12 19:52:58 +0800

    Added app route to bridge

--- app/routes/bridge.js
+++ app/routes/bridge.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const appRouter = demand('routes/app');
 const monitorRouter = demand('routes/monitor');
 const securityViolationRouter = demand('routes/security-violation');
 const maintenanceRouter = demand('routes/maintenance');
 
 const router = express.Router();
 
+router.use(appRouter);
 router.use(monitorRouter);
 router.use(securityViolationRouter);
 router.use(maintenanceRouter);
