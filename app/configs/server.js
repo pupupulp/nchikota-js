0e73a69733f6a7fc8218b00b8be8920b634e4fe0
Eagan Martin <mece.martinece@gmail.com>
2019-03-12 16:07:59 +0800

    Added server config

---
+++ app/configs/server.js
@@ -0,0 +1,35 @@
+const moment = require('moment');
+const date = moment().format('YYYYMMDD');
+
+module.exports = {
+	protocol: 'http',
+	port: 9000,
+	script: 'server/bin/www',
+	whitelists: [
+		'*'
+	],
+	blacklists: [
+		'!213.15.*'
+	],
+	forever: {
+		max: 3,
+		silent: false,
+		killTree: true,
+		minUptime: 2000,
+		spinSleepTime: 1000,
+		sourceDir: './app',
+		watch: true,
+		watchDirectory: './app',
+		logFile: `logs/${date}/log.txt`,
+		outFile: `logs/${date}/output.txt`,
+		errFile: `logs/${date}/error.txt`,
+	},
+	vitals: {
+		autoCheck: 5000,
+		httpHealthy: 200,
+		httpUnhealthy: 503
+	},
+	cpuThreshold: 90,
+	tickThreshold: 500,
+	admin: 'mece.martinece@gmail.com'
+};
