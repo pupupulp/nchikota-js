5dbbeb6d54ffd2cd8e982c4f14479192d1b99f7a
Eagan Martin <mece.martinece@gmail.com>
2019-03-12 15:56:52 +0800

    Added script for forever

---
+++ app/scripts/forever-start.js
@@ -0,0 +1,57 @@
+global.demand = path => require(`${__dirname}/../${path}`);
+
+const forever = require('forever-monitor');
+
+// eslint-disable-next-line no-undef
+const logger = demand('middlewares/logger');
+// eslint-disable-next-line no-undef
+const handler = demand('helpers/handler');
+// eslint-disable-next-line no-undef
+const config = demand('configs');
+
+const child = new(forever.Monitor)(config.server.script, config.server.forever);
+
+child.on('start', (process, data) => {
+	logger.info('Server started with pid: ' + data.pid);
+});
+
+child.on('restart', () => {
+	logger.info('Server restarted: ' + child.times + ' time(s)');
+});
+
+child.on('exit:code', (code) => {
+	logger.error('Server exited with code: ' + code);
+});
+
+child.on('watch:restart', (info) => {
+	logger.info('Server restarted due to: ' + info.file + ' changed');
+});
+
+const gracefulShutdown = () => {
+	logger.info('Gracefully shutting down');
+	process.exit(0);
+};
+
+process.on('SIGTERM', () => {
+	logger.info('Server received SIGTERM');
+	setTimeout(gracefulShutdown, 1000);
+});
+
+process.on('SIGINT', () => {
+	logger.info('Server received SIGINT');
+	setTimeout(gracefulShutdown, 1000);
+});
+
+// eslint-disable-next-line no-unused-vars
+process.on('unhandledRejection', (reason, p) => {
+	throw reason;
+});
+
+process.on('uncaughtException', (error) => {
+	handler.error.handleError(error);
+	if (!handler.error.isTrustedError(error)) {
+		process.exit(1);
+	}
+});
+
+child.start();
