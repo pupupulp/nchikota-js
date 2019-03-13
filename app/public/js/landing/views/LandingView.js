a5767c94aba97f20fd9d06e034a315ae6e75a7f9
Eagan Martin <mece.martinece@gmail.com>
2019-03-13 09:19:27 +0800

    Changed background image

--- app/public/js/landing/views/LandingView.js
+++ app/public/js/landing/views/LandingView.js
@@ -7,10 +7,15 @@ Ext.define('App.views.LandingView', {
 	region: 'center',
 
 	initComponent: function () {
-		var self = this;
+		var self = this,
+			BACKGROUND_IMAGE = '/assets/landing.jpg';
 
 		Ext.apply(self, {
 			bodyPadding: '150 0 0 0',
+			bodyStyle: {
+				backgroundImage: 'url('+BACKGROUND_IMAGE+')',
+				backgroundSize: 'cover'
+			},
 		});
 
 		self.callParent();
