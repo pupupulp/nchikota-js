2506fc1feff245c767fa7b7dd135b1d5de7ac954
Eagan Martin <mece.martinece@gmail.com>
2019-03-14 14:26:16 +0800

    Updated logger middleware to dump to file

--- app/middlewares/logger.js
+++ app/middlewares/logger.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
 
@@ -5,13 +6,29 @@ const logFormat = printf(({ level, message, label, timestamp }) => {
 	return `[${timestamp}] [${label}] [${level.toUpperCase()}]: ${message}`;
 });
 
+const date = moment().format('YYYYMMDD');
+
 const logger = createLogger({
 	format: combine(
 		label({ label: 'SERVER' }),
 		timestamp(),
 		logFormat
 	),
-	transports: [new transports.Console()]
+	transports: [
+		new transports.Console(),
+		new transports.File({
+			filename: `logs/${date}/log.txt`,
+			level: 'info'
+		}),
+		new transports.File({
+			filename: `logs/${date}/output.txt`,
+			level: 'verbose'
+		}),
+		new transports.File({
+			filename: `logs/${date}/error.txt`,
+			level: 'error'
+		}),
+	]
 });
 
 module.exports = logger;
