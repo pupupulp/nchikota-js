78fc97592cc6d0ac668e8d571d9cbd736e277cfa
Eagan Martin <mece.martinece@gmail.com>
2019-03-14 13:53:37 +0800

    Removed minify config

--- app/middlewares/minify.js
+++ app/middlewares/minify.js
@@ -2,10 +2,19 @@ const compression = require('compression');
 const minify = require('express-minify');
 const minifyHtml = require('express-minify-html');
 
-const config = demand('configs');
-
 module.exports = (app) => {
 	app.use(compression());
 	app.use(minify());
-	app.use(minifyHtml(config.minify.html));
+	app.use(minifyHtml({
+		override: true,
+		exception_url: false,
+		htmlMinifier: {
+			removeComments: true,
+			collapseWhitespace: true,
+			collapseBooleanAttributes: true,
+			removeAttributeQuotes: true,
+			removeEmptyAttributes: true,
+			minifyJS: true
+		}
+	}));
 };
